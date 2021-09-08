88e40ec65a9009fc11bb0bb35fb02f66c40ed978
RupeshYadav4 <86247908+rupesh0097@users.noreply.github.com>
2021-09-08 11:34:11 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include  <iostream>
+using namespace std;
+
+int main(){
+	/*//using while loop to print number 1 to 5.
+	int i = 0;
+	while (i<=5){
+	cout << i << endl;
+	++i;
+	}
+
+	//creating a program to to find sum of all positive number using while loop.
+	int number,sum = 0;
+	cout << "Enter the number: ";
+	cin >> number;
+
+	while (number > 0){
+		sum += number;
+
+		cout << "Enter anothe number: ";
+		cin >> number;
+	}
+
+	cout << "The sum of given numbers are; " << sum << endl;
+	
+
+	//C++ program to print 1 to 5 using do-while loop.
+		int j = 0;
+		do{
+			cout << j << endl;
+			++j;
+		}
+	  while (j <=5);     */
+
+	
+	//C++ program to find the sum of positive numbers using do-while loop
+
+	
+
+	return 0;
+}
